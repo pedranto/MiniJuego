81d5f8fbe8b484d89987388a0c0c039d98b5c204
Pedro Antonio Lopez Parras <pedro_01_30601@hotmail.es>
2024-02-22 01:14:28 +0100

    link

--- VegaJuego/Level1.cs
+++ VegaJuego/Level1.cs
@@ -2,9 +2,22 @@ namespace VegaJuego
 {
     public partial class Level1 : Form
     {
+        Link Player;
         public Level1()
         {
             InitializeComponent();
+            IniciarPersonajes();
+        }
+
+        private void IniciarPersonajes()
+        {
+            // Lienzo, coordenadaX, coordenadaY, salud, escudo, arma
+            Player = new Link(this, 0, 0, 30, 0, 0);
+        }
+
+        private void Level1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Player.mover(e);
         }
     }
 }
